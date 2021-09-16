f1ca3fe417d4459889d2fc13d5fbc60da41c339f
dladeira <dladeiratx@gmail.com>
2021-09-16 19:53:41 +0200

    Fixed fonts loading

--- components/layout.js
+++ components/layout.js
@@ -6,9 +6,8 @@ const Layout = (props) => (
         <Head>
             <title>Disco Latinam</title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link href="https://fonts.googleapis.com/css2?family=Unkempt:wght@700&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700,500,400&display=swap" rel="stylesheet" /> 
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&family=Unkempt:wght@700&display=swap" rel="stylesheet" /> 
         </Head>
 
         <Header />
