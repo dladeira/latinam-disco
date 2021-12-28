f5e1fb002bfe801f216a9a530add3953684c2aa0
dladeira <dladeiratx@gmail.com>
2021-12-28 13:10:11 +0000

    Renamed to latinam-disco

--- components/home/jumbotron.js
+++ components/home/jumbotron.js
@@ -4,17 +4,11 @@ import { useUser } from '../../lib/hooks'
 
 export default function Jumbotron() {
     const user = useUser()
-    return (user ? (
+    return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Disco Latinam</h1>
+            <h1 className={styles.title}>Latinam Disco</h1>
             <h2 className={styles.subtitle}>Learn latin <i>with ease</i></h2>
             <Link href="/learn"><button className={styles.button}>intra</button></Link>
         </div>
-    ) : (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Disco Latinam</h1>
-            <h2 className={styles.subtitle}>Learn latin <i>with ease</i></h2>
-            <Link href="/login"><button className={styles.button}>intra</button></Link>
-        </div>
-    ))
+    )
 }
--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Header from './gobal/header'
 const Layout = (props) => (
     <>
         <Head>
-            <title>Disco Latinam</title>
+            <title>Latinam Disco</title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&family=Unkempt:wght@700&display=swap" rel="stylesheet" /> 
