1ede37401696e95e340dfb9ee5f9e7cbb33ee874
dladeira <dladeiratx@gmail.com>
2022-01-09 10:22:44 +0100

    Added article rows

--- pages/learn.js
+++ pages/learn.js
@@ -4,11 +4,34 @@ import { useUser } from '../lib/hooks'
 
 export default function Learn({ articles }) {
     const user = useUser({ redirectTo: '/login' })
-    
+
+    var articleRows = [[], [], [], [], []]
+
+    for (var article of articles) {
+        if (article.index) {
+            articleRows[article.index].push(article)
+        } else {
+            articleRows[0].push(article)
+        }
+    }
+
     return (user ? (
-        <>
-            {articles.map((article) => <Article rawTitle={article.title} rawText={article.text} id={article._id} admin={user.admin} />)}
-        </>
+        <div className="container">
+            {articleRows.map((articleRow) => {
+                console.log(articleRow)
+                return (<div className="row">{articleRow.map((article) => <Article rawTitle={article.title} rawText={article.text} id={article._id} admin={user.admin} />)}</div>)
+            })}
+            <style jsx>{`
+        .container {
+          margin: 50px;
+        }
+        .row {
+            display: flex;
+            flex-direction: row;
+
+        }
+      `}</style>
+        </div>
     ) : (
         <>
         </>
